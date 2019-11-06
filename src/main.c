219489825682875207298093f4c8838c34132431
Markus Haraldseid <markuhar@stud.ntnu.no>
2019-11-06 17:54:42 +0100

    read analog signal from wheel at PD6

--- src/main.c
+++ src/main.c
@@ -17,6 +17,14 @@
 
 void setupCMU(void);
 
+void set_decade(uint16_t val);
+uint8_t conv_ascii(uint16_t val);
+
+uint16_t ms_counter = 0;
+
+// Array for displaying ADC result, initialize to "     mV"
+uint8_t digit_array[7] = { 0x20, 0x20, 0x20, 0x20, 0x20, 'm', 'V' };
+
 int main(void)
 {
 	CHIP_Init();
@@ -29,6 +37,34 @@ int main(void)
 	setExtLed(false);
 
 	while(!setDone());
+
+	uint8_t i;
+	// Temp variable for storing ADC conversion results
+	uint16_t adc_result = 0;
+	uint32_t temp;
+
+
+	/*Setup ADC
+	Timebase bit field = 24, defines ADC warm up period (must be greater than or equal to number of clock cycles in 1us)
+	Prescaler setting = 1: ADC clock = HFPERCLK/2 = 12MHz (ADC clock should be between 32kHz and 13MHz)
+	Oversampling set to 2, no input filter, no need for Conversion Tailgating
+	Warm-up mode = NORMAL (ADC is not kept warmed up between conversions)*/
+
+	ADC0->CTRL = (24 << 16) | (1 << 8);
+
+	ADC0->SINGLECTRL = (2 << 16) | (6 << 8);
+	// Disable ADC interrupts
+	ADC0->IEN = 0x0;
+
+	// Setup Timer to trigger conversions
+	// Set TOP value for Timer0
+	TIMER0->TOP = 24000;
+	// Enable Timer0 overflow interrupt
+	TIMER0->IEN = 1;
+	// Enable TIMER0 interrupt vector in NVIC
+	NVIC_EnableIRQ(TIMER0_IRQn);
+	// Start timer0
+	TIMER0->CMD = 0x1;
 
 	#ifndef DEVICE_SADIE
 	SegmentLCD_Init(false);
@@ -50,6 +86,23 @@ int main(void)
 			setExtLed(true);
 			MIDI_packet input = waitForInput();
 			// TODO: do stuff with input
+			if(ms_counter == 500) {
+
+				// Start Single Conversion
+				ADC0->CMD = 0x1;
+				// Wait for single conversion data to become valid
+				while(!(ADC0->STATUS & (1 << 16)));
+				// Store conversion result
+				adc_result = ADC0->SINGLEDATA;
+
+				// Change hex result to decimal result in mV
+				temp = adc_result*3300;
+				adc_result = temp/4095;
+
+				//SegmentLCD_Write("%u",adc_result);
+
+				ms_counter = 0; // reset counter
+				}
 			handleMIDIEvent(&input, generator_states);
 		}
 		// Connection removed
